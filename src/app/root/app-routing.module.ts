29c965ae78606b2f66d542f92fb38b368eeca349
Artem Oktisyuk <artem@wetelo.com>
2021-05-19 17:36:54 +0300

    [CHANGE] replace app to root folder

--- src/app/root/app-routing.module.ts
+++ src/app/root/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {AuthGuard} from './auth/auth.guard';
-import {NotAuthGuard} from './auth/not-auth.guard';
+import {AuthGuard} from '../auth/auth.guard';
+import {NotAuthGuard} from '../auth/not-auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule),
     canActivate: [AuthGuard],
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule),
     canActivate: [NotAuthGuard],
   },
   {
--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -3,15 +3,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthModule} from '@auth0/auth0-angular';
-import {environment, environment as env} from '../environments/environment';
+import {environment, environment as env} from '../../environments/environment';
 import {HttpClientModule} from '@angular/common/http';
 import {ActionReducerMap, StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {EffectsModule} from '@ngrx/effects';
-import {AuthEffects} from './auth/store/auth.effects';
-import {AUTH_NODE, authReducer} from './auth/store/auth.reducer';
-import {SharedModule} from './shared/shared.module';
-import {DASHBOARD_NODE, dashboardReducer} from './dashboard/store';
+import {AuthEffects} from '../auth/store/auth.effects';
+import {AUTH_NODE, authReducer} from '../auth/store/auth.reducer';
+import {SharedModule} from '../shared/shared.module';
+import {DASHBOARD_NODE, dashboardReducer} from '../dashboard/store';
 
 const reducers: ActionReducerMap<any> = {
   [AUTH_NODE]: authReducer,
